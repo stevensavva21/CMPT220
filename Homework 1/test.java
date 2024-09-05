9e5a37091d351165b0763cd21d0c5ead72f66ec4
stevensavva21 <steven.savva1@marist.edu>
2024-09-05 14:23:44 -0400

    done

---
+++ Homework 1/test.java
@@ -0,0 +1,19 @@
+//always start with importing our scanner so we can use it!
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(12);
+
+        String name = "Steven" ;
+        System.out.println(name);
+        System.out.println(name);
+
+        Integer age = 20;
+
+        System.out.print("My name is " + name  + " i am " + age + " years old.");
+
+
+    }
+}
